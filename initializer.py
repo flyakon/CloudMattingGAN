c0a6bb31d6f786b04b2841cc7efca7a669155a9f
jiupinjia <51430557+jiupinjia@users.noreply.github.com>
2020-05-01 18:27:51 -0400

    Update initializer.py

--- initializer.py
+++ initializer.py
@@ -15,7 +15,7 @@ class TrainingParamInitialization():
         self.img_size = FLAGS.img_size
         self.alpha_channel = FLAGS.alpha_channel
         self.reflectance_channel = FLAGS.reflectance_channel
-        self.img_channel=FLAGS.img_channel
+        self.img_channel = FLAGS.img_channel
 
         self.D_input_size = FLAGS.D_input_size
         self.G_input_size = FLAGS.G_input_size
@@ -24,13 +24,7 @@ class TrainingParamInitialization():
         self.g_learning_rate =FLAGS.g_learning_rate
         self.d_learning_rate = FLAGS.d_learning_rate
 
-        # self.gan_model = 'W_GAN'
-        # self.d_clip = 0.05
-        # self.optimizer = 'RMSProp'
-
         self.gan_model = FLAGS.gan_model
-        # self.d_clip = 1e9
-        # self.optimizer = 'Adam'
         if self.gan_model=='Vanilla_GAN':
             self.d_clip=1e9
             self.optimizer = 'Adam'
@@ -45,24 +39,18 @@ class TrainingParamInitialization():
             exit(-1)
 
         self.image_dir = FLAGS.image_dir
-        self.checkpoint_gan =FLAGS.checkpoint_gan # Directory name to save the checkpoints
+        self.checkpoint_gan = FLAGS.checkpoint_gan # Directory name to save the checkpoints
         self.sample_dir = FLAGS.sample_dir # Directory name to save the samples on training
         self.result_dir = FLAGS.result_dir # Directory name to save the generated images
 
-        self.iter_step=FLAGS.iter_step        #迭代次数
+        self.iter_step = FLAGS.iter_step        # number of training iterations
 
-        self.learning_rate=FLAGS.learning_rate
-        self.num_classes=FLAGS.num_classes
+        self.learning_rate = FLAGS.learning_rate
+        self.num_classes = FLAGS.num_classes
 
-        self.testDataPath=FLAGS.testDataPath
-        self.model_path=FLAGS.model_path
-        self.logdir=FLAGS.logdir
-        self.net_name=FLAGS.net_name
-        self.result_path=FLAGS.result_path
-
-
-
-
-
-
+        self.testDataPath = FLAGS.testDataPath
+        self.model_path = FLAGS.model_path
+        self.logdir = FLAGS.logdir
+        self.net_name = FLAGS.net_name
+        self.result_path = FLAGS.result_path
 
